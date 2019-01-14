b3712c1e68870e1cfeece20d2e121dcfa72babbe
Stephen Brooks <smfbrooks@gmail.com>
2019-01-13 18:21:49 -0800

    remove comments

--- serverV3.py
+++ serverV3.py
@@ -18,58 +18,35 @@ def main():
 	args = getargs()
 	print(args)
 
-	# Create a server socket, bind it to a port and start listening
 	port = 8888
 	sock = socket(AF_INET, SOCK_STREAM)
 	sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	# Fill in start.
-	sock.bind((args.ipaddr, port)) # listening port 
+	sock.bind((args.ipaddr, port))
 	sock.listen(1)
-	# Fill in end.
 	while True:
-		# Start receiving data from the client
 		print(f"Listening on... {args.ipaddr}:{port}")
 		clsock, claddr = sock.accept()
 		print(f"Received a connection from: {claddr}")
 		message = clsock.recv(1024)
-		# print(message)
-		# Extract the filename from the given message
-		# print(message.split()[1])
 		request_type = message.split()[0]
 		filename = message.decode().split()[1].partition("/")[2]
-		# file_exist = "false"
-		# filename = filename
 
-		try: # Check whether the file exist in the cache
+		try:
 			with open(filename[1:-1], "r") as fh:
 				output = fh.readlines()
-			# file_exist = "true"
-			# ProxyServer finds a cache hit and generates a response message
 			clsock.send("HTTP/1.0 200 OK\r\n".encode())
 			clsock.send("Content-Type:text/html\r\n".encode())
-			# Fill in start.
 			for data in output:
 				clsock.send(data.encode())
-			# Fill in end.
 			print("Served cached version")
 			continue
-		# Error handling for file not found in cache
 		except IOError:
 			print("Cache file does not exist")
-		# Create a socket on the proxyserver
-		# Fill in start.
 		c = socket(AF_INET, SOCK_STREAM)
-		# Fill in end.
-		# print(filename)
 		hostn = filename.replace("www.", "", 1)[1:-1]
 		print(f"Attepting connection to: {hostn}")
 		
-		# Connect to the socket to port 80
-		# Fill in start.
 		c.connect((hostn, 80))
-		# Fill in end.
-		# Create a temporary file on this socket and ask port 80
-		# for the file requested by the client
 		fileobj = c.makefile('rwb', 0)
 		print(vars(fileobj))
 		subfile = filename.partition("/")[2]
@@ -79,20 +56,12 @@ def main():
 		else:
 			fileobj.write("".join(["POST ", "http://", filename, " HTTP/1.0\n\n"]).encode())
 			fileobj.write(re.split("\r\n", message.decode(), maxsplit=1)[1].encode())
-		# Read the response into buffer
-		# Fill in start.
 		buf = fileobj.readlines()
 		
-		# HTTP response message for file not found
 		if buf[0].split()[1] == '404':
 			print("Server Response: {}".format(buf[0].split('\n', 1)[0]))
-		# Fill in end.
-		# Create a new file in the cache for the requested file.
-		# Also send the response in the buffer to client socket
-		# and the corresponding file in the cache
 		else:
 			with open(filename[1:-1], "wb") as fh:
-		# Fill in start.
 				for data in buf:
 					fh.write(data)
 					clsock.send(data)
@@ -100,11 +69,8 @@ def main():
 					print("Server Response: {}".format(buf[0].split('\n', 1)[0]))
 				except Exception as e:
 					print(e.__traceback__)
-		# Fill in end.
 
-		# Close the client and the server sockets
 		clsock.close()
-		# Fill in start.
 		c.close()
 	
 if __name__ == '__main__':
